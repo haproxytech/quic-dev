fceec09e9f3fab558d21b3ba8170126134e4f97e
Frédéric Lécaille <flecaille@haproxy.com>
2019-08-13 15:53:33 +0200

    MINOR: quic: Add cryptographic functions for QUIC-TLS.

---
+++ src/quic_tls.c
@@ -0,0 +1,102 @@
+#include <string.h>
+#include <openssl/evp.h>
+#include <openssl/kdf.h>
+
+int quic_hdkf_extract(unsigned char *buf, size_t *buflen,
+                      unsigned char *key, size_t keylen,
+                      unsigned char *salt, size_t saltlen)
+{
+    EVP_PKEY_CTX *ctx;
+
+    ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, NULL);
+    if (!ctx)
+        return 0;
+
+    if (EVP_PKEY_derive_init(ctx) <= 0 ||
+        EVP_PKEY_CTX_hkdf_mode(ctx, EVP_PKEY_HKDEF_MODE_EXTRACT_ONLY) <= 0 ||
+        EVP_PKEY_CTX_set_hkdf_md(ctx, EVP_sha256()) <= 0 ||
+        EVP_PKEY_CTX_set1_hkdf_salt(ctx, salt, saltlen) <= 0 ||
+        EVP_PKEY_CTX_set1_hkdf_key(ctx, key, keylen) <= 0 ||
+        EVP_PKEY_derive(ctx, buf, buflen) <= 0)
+        goto err;
+
+    EVP_PKEY_CTX_free(ctx);
+    return 1;
+
+ err:
+    EVP_PKEY_CTX_free(ctx);
+    return 0;
+}
+
+int quic_hdkf_expand(unsigned char *buf, size_t *buflen,
+                     const unsigned char *key, size_t keylen,
+                     const unsigned char *label, size_t labellen)
+{
+    EVP_PKEY_CTX *ctx;
+
+    ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, NULL);
+    if (!ctx)
+        return 0;
+
+    if (EVP_PKEY_derive_init(ctx) <= 0 ||
+        EVP_PKEY_CTX_hkdf_mode(ctx, EVP_PKEY_HKDEF_MODE_EXPAND_ONLY) <= 0 ||
+        EVP_PKEY_CTX_set_hkdf_md(ctx, EVP_sha256()) <= 0 ||
+        EVP_PKEY_CTX_set1_hkdf_key(ctx, key, keylen) <= 0 ||
+        EVP_PKEY_CTX_add1_hkdf_info(ctx, label, labellen) <= 0 ||
+        EVP_PKEY_derive(ctx, buf, buflen) <= 0)
+        goto err;
+
+    EVP_PKEY_CTX_free(ctx);
+    return 1;
+
+ err:
+    EVP_PKEY_CTX_free(ctx);
+    return 0;
+}
+
+/* https://quicwg.org/base-drafts/draft-ietf-quic-tls.html#protection-keys
+ * refers to:
+ *
+ * https://tools.ietf.org/html/rfc8446#section-7.1:
+ * 7.1.  Key Schedule
+ *
+ * The key derivation process makes use of the HKDF-Extract and
+ * HKDF-Expand functions as defined for HKDF [RFC5869], as well as the
+ * functions defined below:
+ *
+ *     HKDF-Expand-Label(Secret, Label, Context, Length) =
+ *          HKDF-Expand(Secret, HkdfLabel, Length)
+ *
+ *     Where HkdfLabel is specified as:
+ *
+ *     struct {
+ *         uint16 length = Length;
+ *         opaque label<7..255> = "tls13 " + Label;
+ *         opaque context<0..255> = Context;
+ *     } HkdfLabel;
+ *
+ *     Derive-Secret(Secret, Label, Messages) =
+ *          HKDF-Expand-Label(Secret, Label,
+ *                            Transcript-Hash(Messages), Hash.length)
+ *
+ */
+int quic_hdkf_expand_label(unsigned char *buf, size_t *buflen,
+                           const unsigned char *key, size_t keylen,
+                           const unsigned char *label, size_t labellen)
+{
+	unsigned char hdkf_label[256], *pos;
+	const unsigned char hdkf_label_label[] = "tls13 ";
+	size_t hdkf_label_label_sz = sizeof hdkf_label_label - 1;
+
+	pos = hdkf_label;
+	*pos++ = *buflen >> 8;
+	*pos++ = *buflen & 0xff;
+	*pos++ = hdkf_label_label_sz + labellen;
+	memcpy(pos, hdkf_label_label, hdkf_label_label_sz);
+	pos += hdkf_label_label_sz;
+	memcpy(pos, label, labellen);
+	pos += labellen;
+	*pos++ = '\0';
+
+	return quic_hdkf_expand(buf, buflen, key, keylen, hdkf_label, pos - hdkf_label);
+}
