f4657154b3370a2e13988b25500a46edc475b666
Frédéric Lécaille <flecaille@haproxy.com>
2020-10-21 14:56:55 +0200

    BUILD: quic: Remove ->pto field from paquet number space struct.
    Should have come with this commit:
      "MINOR: quic: Minor modification for quic_pto_pktns()"

--- include/proto/xprt_quic.h
+++ include/proto/xprt_quic.h
@@ -954,7 +954,6 @@ static inline void quic_pktns_init(struct quic_pktns *pktns)
 	pktns->tx.largest_acked_pn = -1;
 	pktns->tx.time_of_last_eliciting = 0;
 	pktns->tx.loss_time = TICK_ETERNITY;
-	pktns->tx.pto = TICK_ETERNITY;
 	pktns->tx.in_flight = 0;
 
 	pktns->rx.largest_pn = -1;
