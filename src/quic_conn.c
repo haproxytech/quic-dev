a0dda4b96eda3df643292dfcdc1d7a36865c12c6
Emeric Brun <ebrun@haproxy.com>
2023-09-28 15:29:53 +0200

    Revert "BUG/MEDIUM: quic: missing check of dcid for init pkt including a token"
    This reverts commit 072e77493961a06b89f853f4ab2bbf0e9cf3eff7.
    
    Doing h2load with h3 tests we notice this behavior:
    
    Client ---- INIT no token SCID = a , DCID = A ---> Server (1)
    Client <--- RETRY+TOKEN DCID = a, SCID = B    ---- Server (2)
    Client ---- INIT+TOKEN SCID = a , DCID = B    ---> Server (3)
    Client <--- INIT DCID = a, SCID = C           ---- Server (4)
    Client ---- INIT+TOKEN SCID = a, DCID = C     ---> Server (5)
    
    With (5) dropped by haproxy due to token validation.
    
    Indeed the previous patch adds SCID of retry packet sent to the aad
    of the token ciphering aad. It was useful to validate the next INIT
    packets including the token are sent by the client using the new
    provided SCID for DCID as mantionned into the RFC 9000.
    But this stateless information is lost on received INIT packets
    following the first outgoing INIT packet from the server because
    the client is also supposed to re-use a second time the lastest
    received SCID for its new DCID. This will break the token validation
    on those last packets and they will be dropped by haproxy.
    
    It was discussed there:
    https://mailarchive.ietf.org/arch/msg/quic/7kXVvzhNCpgPk6FwtyPuIC6tRk0/
    
    To resume: this is not the role of the server to verify the re-use of
    retry's SCID for DCID in further client's INIT packets.
    
    The previous patch must be reverted in all versions where it was
    backported (supposed until 2.6)
    
    (cherry picked from commit 3c250cb84767e709d53ca985930cd0fc51605044)
    [wt: 2.8 commit was 0ae0d74d95; reverted by hand since files moved]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/quic_conn.c
+++ src/quic_conn.c
@@ -6387,8 +6387,7 @@ static int send_stateless_reset(struct listener *l, struct sockaddr_storage *dst
  */
 static int quic_generate_retry_token_aad(unsigned char *aad,
                                          uint32_t version,
-                                         const struct quic_cid *dcid,
-                                         const struct quic_cid *scid,
+                                         const struct quic_cid *cid,
                                          const struct sockaddr_storage *addr)
 {
 	unsigned char *p;
@@ -6396,11 +6395,9 @@ static int quic_generate_retry_token_aad(unsigned char *aad,
 	p = aad;
 	*(uint32_t *)p = htonl(version);
 	p += sizeof version;
-	memcpy(p, dcid->data, dcid->len);
-	p += dcid->len;
 	p += quic_saddr_cpy(p, addr);
-	memcpy(p, scid->data, scid->len);
-	p += scid->len;
+	memcpy(p, cid->data, cid->len);
+	p += cid->len;
 
 	return p - aad;
 }
@@ -6415,15 +6412,13 @@ static int quic_generate_retry_token_aad(unsigned char *aad,
 static int quic_generate_retry_token(unsigned char *token, size_t len,
                                      const uint32_t version,
                                      const struct quic_cid *odcid,
-                                     const struct quic_cid *scid,
                                      const struct quic_cid *dcid,
                                      struct sockaddr_storage *addr)
 {
 	int ret = 0;
 	unsigned char *p;
-	unsigned char aad[sizeof(uint32_t) + QUIC_CID_MAXLEN +
-		          sizeof(in_port_t) + sizeof(struct in6_addr) +
-			  QUIC_CID_MAXLEN];
+	unsigned char aad[sizeof(uint32_t) + sizeof(in_port_t) +
+	                  sizeof(struct in6_addr) + QUIC_CID_MAXLEN];
 	size_t aadlen;
 	unsigned char salt[QUIC_RETRY_TOKEN_SALTLEN];
 	unsigned char key[QUIC_TLS_KEY_LEN];
@@ -6443,7 +6438,7 @@ static int quic_generate_retry_token(unsigned char *token, size_t len,
 	if (1 + odcid->len + 1 + sizeof(timestamp) + QUIC_TLS_TAG_LEN + QUIC_RETRY_TOKEN_SALTLEN > len)
 		goto err;
 
-	aadlen = quic_generate_retry_token_aad(aad, version, scid, dcid, addr);
+	aadlen = quic_generate_retry_token_aad(aad, version, dcid, addr);
 	/* TODO: RAND_bytes() should be replaced */
 	if (RAND_bytes(salt, sizeof salt) != 1) {
 		TRACE_ERROR("RAND_bytes()", QUIC_EV_CONN_TXPKT);
@@ -6516,9 +6511,8 @@ static int quic_retry_token_check(struct quic_rx_packet *pkt,
 	unsigned char *token = pkt->token;
 	const uint64_t tokenlen = pkt->token_len;
 	unsigned char buf[128];
-	unsigned char aad[sizeof(uint32_t) + QUIC_CID_MAXLEN +
-		          sizeof(in_port_t) + sizeof(struct in6_addr) +
-			  QUIC_CID_MAXLEN];
+	unsigned char aad[sizeof(uint32_t) + sizeof(in_port_t) +
+	                  sizeof(struct in6_addr) + QUIC_CID_MAXLEN];
 	size_t aadlen;
 	const unsigned char *salt;
 	unsigned char key[QUIC_TLS_KEY_LEN];
@@ -6559,7 +6553,7 @@ static int quic_retry_token_check(struct quic_rx_packet *pkt,
 		goto err;
 	}
 
-	aadlen = quic_generate_retry_token_aad(aad, qv->num, &pkt->dcid, &pkt->scid, &dgram->saddr);
+	aadlen = quic_generate_retry_token_aad(aad, qv->num, &pkt->scid, &dgram->saddr);
 	salt = token + tokenlen - QUIC_RETRY_TOKEN_SALTLEN;
 	if (!quic_tls_derive_retry_token_secret(EVP_sha256(), key, sizeof key, iv, sizeof iv,
 	                                        salt, QUIC_RETRY_TOKEN_SALTLEN, sec, seclen)) {
@@ -6643,7 +6637,7 @@ static int send_retry(int fd, struct sockaddr_storage *addr,
 
 	/* token */
 	if (!(token_len = quic_generate_retry_token(&buf[i], sizeof(buf) - i, qv->num,
-	                                            &pkt->dcid, &scid, &pkt->scid, addr))) {
+	                                            &pkt->dcid, &pkt->scid, addr))) {
 		TRACE_ERROR("quic_generate_retry_token() failed", QUIC_EV_CONN_TXPKT);
 		goto out;
 	}
