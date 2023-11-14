fb79f968bfb24d20be583f8a8c447afe6f890609
Christopher Faulet <cfaulet@haproxy.com>
2023-11-14 07:44:05 +0100

    MINOR: connection: Add a CTL flag to notify mux it should wait for reads again
    MUX_SUBS_RECV ctl flag is added to instruct the mux it should wait for read
    events. This flag will be pretty useful to handle abortonclose option.
    
    (cherry picked from commit e5cffa8ace3b9a3d6a634d8754380e699909ad3a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- include/haproxy/connection-t.h
+++ include/haproxy/connection-t.h
@@ -328,6 +328,7 @@ enum proto_proxy_side {
 enum mux_ctl_type {
 	MUX_STATUS, /* Expects an int as output, sets it to a combinaison of MUX_STATUS flags */
 	MUX_EXIT_STATUS, /* Expects an int as output, sets the mux exist/error/http status, if known or 0 */
+	MUX_SUBS_RECV, /* Notify the mux it must wait for read events again  */
 };
 
 /* response for ctl MUX_STATUS */
