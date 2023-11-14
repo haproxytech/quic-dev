8af12381581fb327dcadf716fb626de6ed618415
Christopher Faulet <cfaulet@haproxy.com>
2023-11-14 19:18:53 +0100

    BUG/MEDIUM: stream: Don't call mux .ctl() callback if not implemented
    The commit 5ff7d2276 ("BUG/MEDIUM: stream: Properly handle abortonclose when set
    on backend only") introduced a regression. Not all multiplexer implement the
    .ctl() callback function. Thus we must be sure this callback function is defined
    first to call it.
    
    This patch should fix a crash reported by Tristan in the issue #2095. It must be
    backported as far as 2.2, with the commit above.
    
    (cherry picked from commit 691f4cf449c700c7abedb845557d39cd788264b0)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/stream.c
+++ src/stream.c
@@ -2314,7 +2314,7 @@ struct task *process_stream(struct task *t, void *context, unsigned int state)
 				if (s->be->options & PR_O_ABRT_CLOSE) {
 					struct connection *conn = sc_conn(scf);
 
-					if (conn)
+					if (conn && conn->mux && conn->mux->ctl)
 						conn->mux->ctl(conn, MUX_SUBS_RECV, NULL);
 				}
 			}
