8b8fb7776eb7b28b07e6d5c3439f3725c9342cb4
Aurelien DARRAGON <adarragon@haproxy.com>
2024-06-04 15:15:29 +0200

    BUG/MINOR: hlua: fix leak in hlua_ckch_set() error path
    in hlua_ckch_commit_yield() and hlua_ckch_set(), when an error occurs,
    we enter the error path and try to raise an error from the <err> msg
    pointer which must be freed afterwards.
    
    However, the fact that luaL_error() never returns was overlooked, because
    of that <err> msg is never freed in such case.
    
    To fix the issue, let's use hlua_pushfstring_safe() helper to push the
    err on the lua stack and then free it before throwing the error using
    lua_error().
    
    It should be backported up to 2.6 with 30fcca18 ("MINOR: ssl/lua:
    CertCache.set() allows to update an SSL certificate file")
    
    (cherry picked from commit 755c2daf0f88885fd6825c55ae59198726c4905e)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/hlua.c
+++ src/hlua.c
@@ -13143,8 +13143,9 @@ __LJMP static int hlua_ckch_commit_yield(lua_State *L, int status, lua_KContext 
 error:
 	ckch_store_free(new_ckchs);
 	HA_SPIN_UNLOCK(CKCH_LOCK, &ckch_lock);
-	WILL_LJMP(luaL_error(L, "%s", err));
+	hlua_pushfstring_safe(L, "%s", err);
 	free(err);
+	WILL_LJMP(lua_error(L));
 
 	return 0;
 }
@@ -13287,7 +13288,9 @@ end:
 
 	if (errcode & ERR_CODE) {
 		ckch_store_free(new_ckchs);
-		WILL_LJMP(luaL_error(L, "%s", err));
+		hlua_pushfstring_safe(L, "%s", err);
+		free(err);
+		WILL_LJMP(lua_error(L));
 	}
 	free(err);
 
