204194f05a01365e9dd312684ac42a13fe0f186a
Frederic Lecaille <flecaille@haproxy.com>
2024-09-05 08:07:49 +0200

    WIP: implement delivery rate sampling algorithm

--- include/haproxy/quic_cc.h
+++ include/haproxy/quic_cc.h
@@ -94,6 +94,7 @@ static inline void quic_cc_path_init(struct quic_cc_path *path, int ipv4, unsign
 	path->in_flight = 0;
 	path->ifae_pkts = 0;
 	quic_cc_init(&path->cc, algo, qc);
+	path->delivery_rate = 0;
 }
 
 /* Return the remaining <room> available on <path> QUIC path for prepared data
