a1e97d62e96986cb76442f8cc6c2211263642af3
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-10-24 16:06:48 +0200

    MINOR: quic_pacing: expose packet rate emission calculation

--- include/haproxy/quic_pacing.h
+++ include/haproxy/quic_pacing.h
@@ -3,10 +3,18 @@
 
 #include <haproxy/quic_pacing-t.h>
 
+#include <haproxy/list.h>
+#include <haproxy/quic_frame-t.h>
+
 static inline void quic_pacing_init(struct quic_pacer *pacer,
                                     const struct quic_cc_path *path)
 {
 	pacer->path = path;
+}
+
+static inline ullong quic_pacing_ns_pkt(const struct quic_pacer *pacer)
+{
+	return pacer->path->loss.srtt * 1000000 / (pacer->path->cwnd / pacer->path->mtu + 1);
 }
 
 #endif /* _HAPROXY_QUIC_PACING_H */
