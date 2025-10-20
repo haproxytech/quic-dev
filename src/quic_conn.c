d2a313b21e3db610597d9aced685be071e3c6cad
Frederic Lecaille <flecaille@haproxy.com>
2025-10-20 10:39:59 +0200

    BUG/MINOR: quic: ignore AGAIN ncbuf err when parsing CRYPTO frames
    This fix follows this previous one:
    
        BUG/MINOR: quic: reorder fragmented RX CRYPTO frames by their offsets
    
    which is not sufficient when a client fragments and mixes its CRYPTO frames AND
    leaveswith holes by packets. ngtcp2 (and perhaps chrome) splits theire CRYPTO
    frames but without hole by packet. In such a case, the CRYPTO parsing leads to
    QUIC_RX_RET_FRM_AGAIN errors which cannot be fixed when the peer resends its packets.
    Indeed, even if the peer resends its frames in a different order, this does not
    help because since the previous commit, the CRYPTO frames are ordered on haproxy side.
    
    This issue was detected thanks to the interopt tests with quic-go as client. This
    client fragments its CRYPTO frames, mixes them, and generate holes, and most of
    the times with the retry test.
    
    To fix this, when a QUIC_RX_RET_FRM_AGAIN error is encountered, the CRYPTO frames
    parsing is not stop. This leaves chances to the next CRYPTO frames to be parsed.
    
    Must be backported as far as 2.6 as the commit mentioned above.

--- src/quic_conn.c
+++ src/quic_conn.c
@@ -3249,7 +3249,7 @@ static int qc_parse_pkt_frms(struct quic_conn *qc, struct quic_rx_packet *pkt,
 	struct quic_frame *frm = NULL;
 	const unsigned char *pos, *end;
 	enum quic_rx_ret_frm ret;
-	int fast_retrans = 0;
+	int fast_retrans = 0, cf_err = 0;
 
 	TRACE_ENTER(QUIC_EV_CONN_PRSHPKT, qc);
 	/* Skip the AAD */
@@ -3514,9 +3514,8 @@ static int qc_parse_pkt_frms(struct quic_conn *qc, struct quic_rx_packet *pkt,
 
 		case QUIC_RX_RET_FRM_AGAIN:
 			TRACE_STATE("AGAIN encountered", QUIC_EV_CONN_PRSHPKT, qc);
-			/* avoid freeing without eb_delete() */
-			frm = NULL;
-			goto err;
+			cf_err = 1;
+			break;
 
 		case QUIC_RX_RET_FRM_DONE:
 			TRACE_PROTO("frame handled", QUIC_EV_CONN_PRSAFRM, qc, frm);
@@ -3533,6 +3532,9 @@ static int qc_parse_pkt_frms(struct quic_conn *qc, struct quic_rx_packet *pkt,
 		eb64_delete(&frm->crypto.offset_node);
 		qc_frm_free(&frm);
 	}
+
+	if (cf_err)
+		goto err;
 
 	/* Error should be returned if some frames cannot be parsed. */
 	BUG_ON(!eb_is_empty(&cf_frms_tree));
